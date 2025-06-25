01cf11aabd373ac8f4065ce67b8e30886b04c36c
Eliana Kruse <eekruse2@wisc.edu>
2025-06-25 11:26:44 -0400

    Update flower API to query space images instead of flowers

--- api/flower.js
+++ api/flower.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   const { day } = req.query;
   const page = (parseInt(day, 10) % 50) + 1;
 
-  const response = await fetch(`https://api.pexels.com/v1/search?query=flower&per_page=1&page=${page}`, {
+  const response = await fetch(`https://api.pexels.com/v1/search?query=space&per_page=1&page=${page}`, {
     headers: {
       Authorization: process.env.YOUR_PEXELS_API_KEY
     }
